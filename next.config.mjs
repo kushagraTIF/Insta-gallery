5e36c9b2b6d84924462ed545f21bcb9187277c75
Kushagra Krishna <kushagrakrishna@Kushagras-Laptop.local>
2024-08-27 22:45:19 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["space.levo.so"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "scontent.cdninstagram.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
